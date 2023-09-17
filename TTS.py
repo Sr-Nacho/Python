93368f2ed0fb8bff99606714216ed46e576e72bf
Sr-Nacho <126293026+Sr-Nacho@users.noreply.github.com>
2023-09-17 13:50:32 -0600

    TTS.py V1.1.0

--- TTS.py
+++ TTS.py
@@ -1,15 +1,17 @@
 """
 By Miles Elkins
+Below is kind of a long story, so skip it if you feel like it
 With help from ChatGPT to do the if/elif/else statements
 This made it so that inputs were male/female rather than voices[0].id/voices[1].id
 ChatGPT did not contribute any important parts of the code, it simply increased user friendliness
+Because of this, I went out to learn if/elif/else ststements, and now i can do things like this myself
 """
 
 # Set up the library
 import pyttsx3
 engine = pyttsx3.init()
 
-# Choose the voice based on user input
+# Choose the voice based on user input(ChatGPT assisted)
 gender_input = input("Enter 'male' or 'female' to pick a voice: ")
 
 if gender_input == 'male':
@@ -19,8 +21,25 @@ elif gender_input == 'female':
 else:
     print("Voice not found for the specified gender.")
 
-# Say the words
-words = input("What should I say? ")
+#Choose to save as mp3 or say as words 
+print("Save as mp3 or say immediately? ")
+output_type = input("mp3/say: ")
 
-engine.say(words)
-engine.runAndWait()
+#Save/Say
+if output_type == "say":
+    #Say the words
+    words = input("What should I say? ")
+    
+    engine.say(words)
+    engine.runAndWait()
+elif output_type == "mp3":
+    #Save as mp3
+    words = input("What should I save? ")
+    file_name = input("What should it be called? ")
+    
+    engine.save_to_file(words, file_name)
+    engine.runAndWait()
+else:
+    print("That is not a valid response.")
+    engine.say("That is not a valid response")
+    engine.runAndWait()
