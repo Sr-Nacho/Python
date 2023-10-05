81264eaa2687227f5b11359daafe7ff50d00081d
Sr-Nacho <126293026+Sr-Nacho@users.noreply.github.com>
2023-10-04 19:59:28 -0600

    base_10_to_base_x.py V1.3
    added a reverse to base 10 function

--- base_10_to_base_x.py
+++ base_10_to_base_x.py
@@ -16,6 +16,23 @@ def decimal_to_base_n(number, base):
     
     return result
 
+## Reverse of the above function ##
+def base_n_to_decimal(number, base):
+    if base < 2 or base > 36:
+        return "Base out of range (2-36)"
+    
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = 0
+    
+    # Reverse the input number as we process it from right to left
+    number = number[::-1]
+    
+    for i in range(len(number)):
+        digit = digits.index(number[i])
+        result += digit * (base ** i)
+    
+    return result
+
 # Input from the user
 num = int(input('Input data as a number in base 10: '))
 base_x = int(input('Enter the base you want to convert to: '))
@@ -24,3 +41,12 @@ base_x = int(input('Enter the base you want to convert to: '))
 result = decimal_to_base_n(num, base_x)
 
 print(f"The number {num} in base 10 is equivalent to {result} in base {base_x}.")
+
+# Reverse of the above
+num = input('Input data as a number in the specified base: ')
+base_x = int(input('Enter the base of the input number: '))
+
+# Call the function to convert
+result = base_n_to_decimal(num, base_x)
+
+print(f"The number {num} in base {base_x} is equivalent to {result} in base 10.")
