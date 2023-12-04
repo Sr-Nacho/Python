f06998d336a24edd07902022fb7211e7b4097580
Sr-Nacho <126293026+Sr-Nacho@users.noreply.github.com>
2023-12-04 12:50:01 -0700

    Update task_manager.py

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -1,3 +1,4 @@
+## The actual project
 ## Set up variables and library
 # Import libraries and set up variables
 import time
@@ -9,6 +10,8 @@ complete_tasks = []
 report = 0
 points = 0
 ranks = []
+achievement = []
+streak = 0
 
 # Welcome the user with a start message
 # The user should only see the start message once
@@ -64,7 +67,7 @@ while active:
         # Prevent confusion by showing the user the new task order
         # Print tasks
         for i in range(len(tasks)):
-            print("Task " + str(i + 1) + " " + tasks[i])
+            print("    Task " + str(i + 1) + " " + tasks[i])
             time.sleep(1)
     
     
@@ -75,24 +78,21 @@ while active:
         complete_tasks.append(tasks[done])
         del tasks[done]
         points += 100
-        print("Points = " + str(points))
-        
         
     if delete_bool and done_bool:
         print("New task list: ")
         # Prevent confusion by showing the user the new task order
         # Print tasks
         for i in range(len(tasks)):
-            print("Task " + str(i + 1) + " " + tasks[i])
+            print("    Task " + str(i + 1) + " " + tasks[i])
             time.sleep(1)
-            points -= 25
-            print("Points = " + str(points))
         
     # Delete tasks
     for i in range(delete_num):
         delete = int(input("Which task do you want to delete? Enter task number "))
         delete -= 1
         del tasks[delete]
+        points -= 25
         
         # Prevent confusion by showing the user the new task order
         if delete_num > 1:
@@ -100,8 +100,10 @@ while active:
             
             # Print tasks
             for i in range(len(tasks)):
-                print("Task " + str(i + 1) + " " + tasks[i])
+                print("    Task " + str(i + 1) + " " + tasks[i])
                 time.sleep(1)
+                
+    print("Points = " + str(points))
         
     # Optional task display
     see_tasks = input("Would you like to see your tasks? y/n ").lower() == "y"
@@ -109,21 +111,129 @@ while active:
         
         # Print tasks
         for i in range(len(tasks)):
-            print("Task " + str(i + 1) + " " + tasks[i])
+            print("    Task " + str(i + 1) + " " + tasks[i])
             time.sleep(1)
         if len(complete_tasks) > 0:
             print("Completed tasks: ")
             time.sleep(1)
             for i in range(len(complete_tasks)):
-                print("Complete task " + str(i + 1) + " " + complete_tasks[i])
+                print("    Complete task " + str(i + 1) + " " + complete_tasks[i])
                 time.sleep(1)
+                
+    report += 1
+    
+    # Achievements
+    if "SSS+" in ranks and "Cheater" not in achievement:
+        achievement.append("Cheater")
+        print("""
+            Achievement unlocked:
             
+               >--Cheater--<
+               
+                   Cheat
+            
+            """)
+    if "SSS" in ranks and "Max Power" not in achievement:
+        achievement.append("Max power")
+        print("""
+            Achievement unlocked:
+            
+               >--Max Power--<
+                   
+               Reach SSS rank
+        
+        """)
+    if points < 0 and report == 5 and "Sub-Zero" not in achievement:
+        achievement.append("Sub-Zero")
+        print("""
+            Achievement unlocked:
+            
+                >--Sub-Zero--<
+                
+                Get a negative 
+                score. It's cold,
+                but fair
+                
+        """)
+    if "F" in ranks and "D" in ranks and "C" in ranks and "B" in ranks and "A" in ranks and "S" in ranks and "SS" in ranks and "SSS" in ranks and "I've seen it all" not in achievements:
+        achievement.append("I've seen it all")
+        print("""
+            Achievement unlocked:
+            
+           >--I've seen it all--<
+           
+               Get all of the
+               ranks
+               
+        """)
+    if streak == 5 and "Streak" not in achievements:
+        print("""
+            Achievement unlocked:
+                
+                >--Streak--<
+                
+                Get B rank or
+                higher 5 times
+                in a row
+                
+        """)
+    if rank == "A" and "Ace" not in achievements:
+        print("""
+            Achievement unlocked:
+            
+                >--Ace--<
+                
+                Get an A
+                rank
+        """)
+                
+    # Progress reports
+    if report == 5:
+        report = 0
+        if points < 100:
+            rank = "F"
+            print("Rank " + rank)
+            streak = 0
+        elif points <= 200:
+            rank = "D"
+            print("Rank " + rank)
+            streak = 0
+        elif points <= 300:
+            rank = "C"
+            print("Rank " + rank)
+            streak = 0
+        elif points <= 400:
+            rank = "B"
+            print("Rank " + rank)
+            streak += 1
+        elif points <= 550:
+            rank = "A"
+            print("Rank " + rank)
+            streak += 1
+        elif points <= 750:
+            rank = "S"
+            print("Rank " + rank)
+            streak += 1
+        elif points <= 1000:
+            rank = "SS"
+            print("Rank " + rank)
+            streak += 1
+        elif points > 1000:
+            rank = "SSS"
+            print("Rank " + rank)
+            streak += 1
+        else:
+            # You have to cheat to get this
+            rank = "SSS+"
+            
+        ranks.append(rank)
+        points = 0
+    
     print("Going home...")
     
     # Pause before sending back to home screen
     time.sleep(5)
     
-    report += 1
-    
-## Used to identify errors
+# Used to identify errors
+# Since the while loop never ends, the else will never run
 else: print("Something broke")
